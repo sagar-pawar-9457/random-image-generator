a16497f5f608e43aedf532d4433a30f77608e613
Sagar Pawar <163662611+sagar-pawar-9457@users.noreply.github.com>
2025-02-09 19:53:39 +0000

    project will be done

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const btnEl = document.querySelector(".btn");
-
-btnEl.addEventListener("mouseover",(e)=>{
-    btnEl.computedStyleMap.
-    const x = e.pageX - btnEl.offsetLeft;
-    const Y = e.pageY - btnEl.offsetTop;
-    
-    
+const imgContainer = document.querySelector(".img-container");
+const btnE1 = document.querySelector(".btn");
+btnE1.addEventListener("click",()=>{
+    imgNum = 6
+    updateImg()
 })
+
+function updateImg(){
+    for(let i =0; i < imgNum; i++){
+        const newImg = document.createElement("img")
+    imgContainer.appendChild(newImg)
+    newImg.src = `https://picsum.photos/300?random=${Math.random()*1000}`
+    }
+}
+    
