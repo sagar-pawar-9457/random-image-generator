50cb6da558b52b8635503b31270d6771a52e8665
Sagar Pawar <163662611+sagar-pawar-9457@users.noreply.github.com>
2025-02-10 00:58:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover",(e)=>{
+    btnEl.computedStyleMap.
+    const x = e.pageX - btnEl.offsetLeft;
+    const Y = e.pageY - btnEl.offsetTop;
+    
+    
+})
